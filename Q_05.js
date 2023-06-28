59791097704c0e8a0d2ad505bc4309c77f44b34b
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-06-28 22:20:30 +0530

    Q_05.js

---
+++ Q_05.js
@@ -0,0 +1,14 @@
+var arrangeCoins = function(n) {
+    let start = 0;
+    let end = n+1;
+    while(start < end){
+        let mid = start+Math.floor((end-start)/2)
+        let product = ((mid+1)*(mid+2))/2
+        if(product > n) {
+          end = mid  
+        } else {
+            start = mid+1
+        }
+    }
+    return start
+};
